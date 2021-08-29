8368309867fb5306b6a7423a71d928b91d188bdf
Andrey Shemet <an-shem@ukr.net>
2021-08-29 17:07:19 +0300

    Удалил работу приложения с Local Storage

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 import './index.css';
 import App from './components/phonebookApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </PersistGate>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,51 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
 import items from './slice-items';
 import filter from './slice-filter';
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
-
-const rootPersistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['contacts'],
-};
 
 const contacts = combineReducers({
   items,
   filter,
 });
 
-const rootReducer = combineReducers({
-  contacts: persistReducer(persistConfig, contacts),
-});
-
-const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: contacts,
   devTools: process.env.NODE_ENV !== 'production',
 });
-
-export const persistor = persistStore(store);
