2bca944f33ac0d4d98a2887c70ea1b42fa1543b2
Andrey Shemet <an-shem@ukr.net>
2021-08-30 16:06:57 +0300

    Заменил Reducer на Slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contacts from '../redux/contactReducer';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import items from '../redux/sliceContacts';
+import filter from '../redux/sliceFilter';
+import error from '../redux/sliceError';
+
+const contacts = combineReducers({
+  items,
+  filter,
+  error,
+});
 
 export const store = configureStore({
   reducer: {
