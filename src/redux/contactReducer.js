b6a18b14061ade21bf76b538fb3f3cb6c715a5ae
Andrey Shemet <an-shem@ukr.net>
2021-08-30 15:40:38 +0300

    удалил состояние загрузки из стейта

--- src/redux/contactReducer.js
+++ src/redux/contactReducer.js
@@ -13,18 +13,6 @@ export const items = createReducer([], {
     state.filter(contact => contact.id !== payload),
 });
 
-export const isLoading = createReducer(false, {
-  [fetchAllContacts.pending]: () => true,
-  [fetchAllContacts.fulfilled]: () => false,
-  [fetchAllContacts.rejected]: () => false,
-  [setContactApi.pending]: () => true,
-  [setContactApi.fulfilled]: () => false,
-  [setContactApi.rejected]: () => false,
-  [deleteContactApi.pending]: () => true,
-  [deleteContactApi.fulfilled]: () => false,
-  [deleteContactApi.rejected]: () => false,
-});
-
 export const error = createReducer(null, {
   [fetchAllContacts.rejected]: (_, { payload }) => payload,
   [setContactApi.rejected]: (_, { payload }) => payload,
@@ -37,7 +25,6 @@ export const error = createReducer(null, {
 const contacts = combineReducers({
   items,
   filter,
-  isLoading,
   error,
 });
 export default contacts;
